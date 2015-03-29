0186be7e5af7518542f43efba6c3d75bacefd001
Brad Campbell <bradjc5@gmail.com>
2015-03-28 20:14:17 -0400

    coap leds for setting leds

---
+++ contiki/apps/coap_leds/coap_leds.c
@@ -0,0 +1,267 @@
+#include "contiki-net.h"
+#include "contiki.h"
+#include "cpu.h"
+#include "sys/etimer.h"
+#include "sys/rtimer.h"
+#include "dev/leds.h"
+#include "dev/uart.h"
+#include "dev/watchdog.h"
+#include "dev/serial-line.h"
+#include "dev/sys-ctrl.h"
+#include "dev/pwm.h"
+#include "net/rime/broadcast.h"
+#include "net/ip/uip.h"
+#include "net/ip/uip-udp-packet.h"
+#include "net/ip/uiplib.h"
+#include "net/ipv6/uip-ds6-route.h"
+#include "net/ipv6/uip-ds6-nbr.h"
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include "rest-engine.h"
+
+
+#define SW_VERSION "1.0"
+#define HW_VERSION "A"
+
+
+PROCESS(app, "CoAP LEDs app");
+AUTOSTART_PROCESSES(&app);
+
+int
+coap_parse_bool (void* request)
+{
+  int length;
+  const char* payload = NULL;
+
+  length = REST.get_request_payload(request, (const uint8_t**) &payload);
+  if (length > 0) {
+    if (strncmp(payload, "true", length) == 0) {
+      return 1;
+    } else if (strncmp(payload, "false", length) == 0) {
+      return 0;
+    } else {
+      return -1;
+    }
+  } else {
+    return -1;
+  }
+}
+
+
+/*******************************************************************************
+ * led0/Power
+ ******************************************************************************/
+
+static void
+led0_power_get_handler(void *request,
+                       void *response,
+                       uint8_t *buffer,
+                       uint16_t preferred_size,
+                       int32_t *offset) {
+  int length;
+
+  length = snprintf((char*) buffer, REST_MAX_CHUNK_SIZE, "%s",
+    ((leds_get()&LEDS_GREEN)==LEDS_GREEN)?"true":"false");
+
+  REST.set_header_content_type(response, REST.type.TEXT_PLAIN);
+  REST.set_response_payload(response, buffer, length);
+}
+
+static void
+led0_power_post_handler(void *request,
+                  void *response,
+                  uint8_t *buffer,
+                  uint16_t preferred_size,
+                  int32_t *offset) {
+
+  uint8_t b = coap_parse_bool(request);
+  if (b == 1) {
+    leds_on(LEDS_GREEN);
+  } else if (b == 0) {
+    leds_off(LEDS_GREEN);
+  } else {
+    REST.set_response_status(response, REST.status.BAD_REQUEST);
+  }
+}
+
+/* A simple actuator example. Toggles the red led */
+RESOURCE(coap_led0_power,
+         "title=\"Green LED\";rt=\"Control\"",
+         led0_power_get_handler,
+         led0_power_post_handler,
+         led0_power_post_handler,
+         NULL);
+
+
+/*******************************************************************************
+ * led1/Power
+ ******************************************************************************/
+
+static void
+led1_power_get_handler(void *request,
+                       void *response,
+                       uint8_t *buffer,
+                       uint16_t preferred_size,
+                       int32_t *offset) {
+  int length;
+
+  length = snprintf((char*) buffer, REST_MAX_CHUNK_SIZE, "%s",
+    ((leds_get()&LEDS_RED)==LEDS_RED)?"true":"false");
+
+  REST.set_header_content_type(response, REST.type.TEXT_PLAIN);
+  REST.set_response_payload(response, buffer, length);
+}
+
+static void
+led1_power_post_handler(void *request,
+                  void *response,
+                  uint8_t *buffer,
+                  uint16_t preferred_size,
+                  int32_t *offset) {
+
+  uint8_t b = coap_parse_bool(request);
+  if (b == 1) {
+    leds_on(LEDS_RED);
+  } else if (b == 0) {
+    leds_off(LEDS_RED);
+  } else {
+    REST.set_response_status(response, REST.status.BAD_REQUEST);
+  }
+}
+
+/* A simple actuator example. Toggles the red led */
+RESOURCE(coap_led1_power,
+         "title=\"Red LED\";rt=\"Control\"",
+         led1_power_get_handler,
+         led1_power_post_handler,
+         led1_power_post_handler,
+         NULL);
+
+
+/*******************************************************************************
+ * led2/Power
+ ******************************************************************************/
+
+static void
+led2_power_get_handler(void *request,
+                       void *response,
+                       uint8_t *buffer,
+                       uint16_t preferred_size,
+                       int32_t *offset) {
+  int length;
+
+  length = snprintf((char*) buffer, REST_MAX_CHUNK_SIZE, "%s",
+    ((leds_get()&LEDS_BLUE)==LEDS_BLUE)?"true":"false");
+
+  REST.set_header_content_type(response, REST.type.TEXT_PLAIN);
+  REST.set_response_payload(response, buffer, length);
+}
+
+static void
+led2_power_post_handler(void *request,
+                  void *response,
+                  uint8_t *buffer,
+                  uint16_t preferred_size,
+                  int32_t *offset) {
+
+  uint8_t b = coap_parse_bool(request);
+  if (b == 1) {
+    leds_on(LEDS_BLUE);
+  } else if (b == 0) {
+    leds_off(LEDS_BLUE);
+  } else {
+    REST.set_response_status(response, REST.status.BAD_REQUEST);
+  }
+}
+
+/* A simple actuator example. Toggles the red led */
+RESOURCE(coap_led2_power,
+         "title=\"Blue LED\";rt=\"Control\"",
+         led2_power_get_handler,
+         led2_power_post_handler,
+         led2_power_post_handler,
+         NULL);
+
+
+/*******************************************************************************
+ * device/software/Version
+ ******************************************************************************/
+
+static void
+device_software_version_get_handler(void *request,
+                  void *response,
+                  uint8_t *buffer,
+                  uint16_t preferred_size,
+                  int32_t *offset) {
+  int length;
+  char res[] = "%s";
+
+  length = snprintf((char*) buffer, REST_MAX_CHUNK_SIZE, res, SW_VERSION);
+
+  REST.set_header_content_type(response, REST.type.TEXT_PLAIN);
+  REST.set_response_payload(response, buffer, length);
+}
+
+RESOURCE(coap_device_software_version,
+         "title=\"device/software/Version\";rt=\"sw\"",
+         device_software_version_get_handler,
+         NULL,
+         NULL,
+         NULL);
+
+
+/*******************************************************************************
+ * device/hardware/Version
+ ******************************************************************************/
+
+static void
+device_hardware_version_get_handler(void *request,
+                  void *response,
+                  uint8_t *buffer,
+                  uint16_t preferred_size,
+                  int32_t *offset) {
+  int length;
+  char res[] = "%s";
+
+  length = snprintf((char*) buffer, REST_MAX_CHUNK_SIZE, res, HW_VERSION);
+
+  REST.set_header_content_type(response, REST.type.TEXT_PLAIN);
+  REST.set_response_payload(response, buffer, length);
+}
+
+RESOURCE(coap_device_hardware_version,
+         "title=\"device/hardware/Version\";rt=\"hw\"",
+         device_hardware_version_get_handler,
+         NULL,
+         NULL,
+         NULL);
+
+
+
+
+
+
+
+PROCESS_THREAD(app, ev, data) {
+  PROCESS_BEGIN();
+
+  leds_on(LEDS_ALL);
+
+  // CoAP + REST
+  rest_init_engine();
+
+  rest_activate_resource(&coap_led0_power,         "led0/Power");
+  rest_activate_resource(&coap_led1_power,         "led1/Power");
+  rest_activate_resource(&coap_led2_power,         "led2/Power");
+
+  rest_activate_resource(&coap_device_software_version, "device/software/Version");
+  rest_activate_resource(&coap_device_hardware_version, "device/hardware/Version");
+
+
+  while (1) {
+    PROCESS_WAIT_EVENT();
+  }
+
+  PROCESS_END();
+}
---
+++ contiki/apps/coap_leds/project-conf.h
@@ -0,0 +1,43 @@
+#ifndef PROJECT_CONF_H_
+#define PROJECT_CONF_H_
+
+/*
+ *
+ * @author: Brad Campbell <bradjc@umich.edu>
+ */
+
+#define NETSTACK_CONF_RDC     nullrdc_driver
+#define NETSTACK_CONF_MAC     nullmac_driver
+
+
+// Shut the thing up
+#define STARTUP_CONF_VERBOSE 1
+//#define UART_CONF_ENABLE 0
+//#define CC2538_CONF_QUIET 1
+
+
+// Do not use some address set by the code, read it in from the magical info
+// page on the cc2538
+#define IEEE_ADDR_CONF_HARDCODED 0
+#define IEEE_ADDR_CONF_USE_SECONDARY_LOCATION 1
+
+// Set the RF channel and panid
+// #define IEEE802154_CONF_PANID 0x689a
+//#define IEEE802154_CONF_PANID 0xbbb1
+//#define CC2538_RF_CONF_CHANNEL 20
+#define IEEE802154_CONF_PANID 0x0022
+#define CC2538_RF_CONF_CHANNEL 19
+
+
+
+// RPL
+// #define RPL_CONF_OF rpl_mrhof
+#define RPL_CONF_OF rpl_of0
+
+#define UIP_CONF_LOGGING 0
+
+
+
+#endif /* PROJECT_CONF_H_ */
+
+/** @} */
