0c2e38a5143c892a29dfc6355055e5ad1104e670
Brad Campbell <bradjc5@gmail.com>
2015-05-15 18:08:14 -0400

    router app

---
+++ contiki/apps/router/router.c
@@ -0,0 +1,91 @@
+
+#include "contiki.h"
+#include "cpu.h"
+#include "sys/etimer.h"
+#include "sys/rtimer.h"
+#include "dev/leds.h"
+#include "dev/uart.h"
+#include "dev/button-sensor.h"
+#include "dev/watchdog.h"
+#include "dev/serial-line.h"
+#include "dev/sys-ctrl.h"
+#include "net/rime/broadcast.h"
+#include "net/ip/uip.h"
+#include "net/ip/uip-udp-packet.h"
+#include "net/ip/uiplib.h"
+#include "net/ipv6/uip-ds6-route.h"
+#include "net/ipv6/uip-ds6-nbr.h"
+
+#include <stdio.h>
+#include <stdint.h>
+/*---------------------------------------------------------------------------*/
+#define LOOP_INTERVAL       CLOCK_SECOND
+#define LEDS_OFF_HYSTERISIS (RTIMER_SECOND >> 1)
+#define LEDS_PERIODIC       LEDS_YELLOW
+#define LEDS_BUTTON         LEDS_RED
+#define LEDS_SERIAL_IN      LEDS_ORANGE
+#define LEDS_REBOOT         LEDS_ALL
+#define LEDS_RF_RX          (LEDS_YELLOW | LEDS_ORANGE)
+#define BROADCAST_CHANNEL   129
+/*---------------------------------------------------------------------------*/
+#define MACDEBUG 0
+
+#define DEBUG 1
+#if DEBUG
+#include <stdio.h>
+#define PRINTF(...) printf(__VA_ARGS__)
+#define PRINT6ADDR(addr) PRINTF(" %02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x ", ((uint8_t *)addr)[0], ((uint8_t *)addr)[1], ((uint8_t *)addr)[2], ((uint8_t *)addr)[3], ((uint8_t *)addr)[4], ((uint8_t *)addr)[5], ((uint8_t *)addr)[6], ((uint8_t *)addr)[7], ((uint8_t *)addr)[8], ((uint8_t *)addr)[9], ((uint8_t *)addr)[10], ((uint8_t *)addr)[11], ((uint8_t *)addr)[12], ((uint8_t *)addr)[13], ((uint8_t *)addr)[14], ((uint8_t *)addr)[15])
+#define PRINTLLADDR(lladdr) PRINTF(" %02x:%02x:%02x:%02x:%02x:%02x ",lladdr->addr[0], lladdr->addr[1], lladdr->addr[2], lladdr->addr[3],lladdr->addr[4], lladdr->addr[5])
+#else
+#define PRINTF(...)
+#define PRINT6ADDR(addr)
+#endif
+
+#define PING6_NB 5
+#define PING6_DATALEN 16
+
+#define UIP_IP_BUF   ((struct uip_ip_hdr *)&uip_buf[UIP_LLH_LEN])
+#define UIP_ICMP_BUF ((struct uip_icmp_hdr *)&uip_buf[uip_l2_l3_hdr_len])
+
+
+static struct etimer periodic_timer;
+
+PROCESS(router_test_process, "Router Process");
+AUTOSTART_PROCESSES(&router_test_process);
+
+
+static void
+recv_handler(void)
+{
+
+}
+
+/*---------------------------------------------------------------------------*/
+static void
+send_handler(process_event_t ev, process_data_t data)
+{
+
+}
+
+/*---------------------------------------------------------------------------*/
+PROCESS_THREAD(router_test_process, ev, data)
+{
+
+  PROCESS_BEGIN();
+
+ // etimer_set(&periodic_timer, 10*CLOCK_SECOND);
+
+  while(1) {
+    PROCESS_YIELD();
+
+    if (etimer_expired(&periodic_timer)) {
+      send_handler(ev, data);
+      etimer_restart(&periodic_timer);
+    } else if (ev == tcpip_event) {
+      recv_handler();
+    }
+  }
+
+  PROCESS_END();
+}
+/*---------------------------------------------------------------------------*/
